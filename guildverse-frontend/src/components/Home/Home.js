627a2ee17dbf6ac6038aa81f1a46be2a1efba9ae
Vitaly Kharlakov <vkharlakov@yandex.ru>
2023-08-11 16:36:06 +0200

    Ref: redesign .home__button

--- guildverse-frontend/src/components/Home/Home.js
+++ guildverse-frontend/src/components/Home/Home.js
@@ -5,9 +5,15 @@ function Home() {
     return (
         <section className="home">
             <p className="home__brief">Web application designed to make raiding easier</p>
-            <button className="home__button">about us</button>
-            <Link className="home__link" to={'/guilds'}>browse guilds</Link>
-            <button className="home__button">how to use</button>
+            <div className='home__button-container'>
+                <button className="home__button">about us</button>
+            </div>
+            <div className='home__button-container'>
+                <Link className="home__link" to={'/guilds'}>browse guilds</Link>
+            </div>
+            <div className='home__button-container'>
+                <button className="home__button">how to use</button>
+            </div>
         </section>
     )
 }
