57d2834b209934dadad14c2bb72b2a975282fc42
Vitaly Kharlakov <vkharlakov@yandex.ru>
2023-08-15 17:12:26 +0200

    Fix: backend

--- guildverse-backend/app.js
+++ guildverse-backend/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const router = require('./routes/router')
 
-const { PORT = 4000 } = process.env
+const { PORT = 3000 } = process.env
 
 const app = express()
 
@@ -12,6 +12,6 @@ mongoose.connect('mongodb://127.0.0.1/guildversedb')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', router)
+app.use(router)
 
 app.listen(PORT, () => { console.log(`App's listening on port ${PORT}`) })
