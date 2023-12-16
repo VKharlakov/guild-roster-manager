ea6022e34a34c88baf4743bc87ba55d7a21c1fd9
Vitaly Kharlakov <vkharlakov@Vitalys-MacBook-Air.local>
2023-12-16 17:36:46 +0100

    Add Form component

---
+++ frontend/src/stories/Form.stories.js
@@ -0,0 +1,9 @@
+import Form from "../components/ui/Form/Form";
+
+export default {
+  title: "components/Form",
+  component: Form,
+};
+
+const Template = (args) => <Form {...args} />;
+export const AddCharacterForm = Template.bind({});
